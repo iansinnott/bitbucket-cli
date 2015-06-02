c85ed417d848b05d476669c9f604da6ed685041c
Ian Sinnott <ian@iansinnott.com>
2015-06-01 22:48:43 -0700

    Require configuration from local config file

--- config.js
+++ config.js
@@ -1,8 +1,26 @@
+var path = require('path');
+var _ = require('lodash');
+
 var BASE_ENDPOINT = 'https://bitbucket.org/api/1.0';
 
-var ENDPIONTS = {
+var ENDPOINTS = {
+  list: BASE_ENDPOINT + '/user/repositories',
   repositories: BASE_ENDPOINT + '/repositories'
 };
 
-module.exports = ENDPIONTS;
+var CREDENTIALS;
+
+try {
+  CREDENTIALS = require(path.join(process.env.HOME, '.bitbucketrc'));
+} catch (e) {
+  console.error("Error: Could not read configuration file at ~/.bitbucketrc");
+  process.exit(1);
+}
+
+if (!CREDENTIALS.username || !CREDENTIALS.password) {
+  console.error('It appears your username or password is not present in your ~/.bitbucketrc file. Please add a username and password and try again.');
+  process.exit(1);
+}
+
+module.exports = _.extend({}, CREDENTIALS, { endpoints: ENDPOINTS });
 
